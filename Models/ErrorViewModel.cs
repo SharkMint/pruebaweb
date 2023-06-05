e4fb5130eb32a263eb745227a66fab527fcaee4f
Maicol Jaquez <maicol120205@gmail.com>
2023-06-05 10:47:47 +0100

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace puebaweb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
